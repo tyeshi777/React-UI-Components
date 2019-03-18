fd0eeb57341d6db76ed3e9138f974edab769dc27
Yeshi <tyeshi777@yahoo.com>
2019-03-18 15:28:31 -0700

    half way

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,8 @@
-import React from 'react';
-import './Card.css';
+import React from "react";
+import "./Card.css";
+
+const CardBanner = () => {
+  return <section className="cardImage" />;
+};
+
+export default CardBanner;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,21 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
+import ImageThumbnail from "./ImageThumbnail";
+import HeaderTitle from "./HeaderTitle";
+import HeaderContent from "./HeaderContent";
+
+const HeaderContainer = () => {
+  return (
+    <div className="headerDiv">
+      <section>
+        <ImageThumbnail />
+      </section>
+      <section>
+        <HeaderTitle />
+        <HeaderContent />
+      </section>
+    </div>
+  );
+};
+
+export default HeaderContainer;
